351baf13539208ed2dc1a028998dfedb80b625c0
Mustafa Dane <mustafadane@gmail.com>
2018-09-18 11:56:14 -0500

    stuck at ls

--- bash.js
+++ bash.js
@@ -1,14 +1,22 @@
 //Output a prompt
+let pwdFunc = require('./pwd.js')
+let lsFunc = require('./ls.js')
 process.stdout.write('type somthing >');
+
 
 //This stdin 'data' even fires after a user types in a line
 process.stdin.on('data', (data) => {
     const cmd = data.toString().trim();
-    if(cmd === 'pwd'){
-        process.stdout.write(process.cwd())
-    }else{
+    if (cmd === 'pwd'){
+        pwdFunc();
+    }
+    else if (cmd === 'ls'){
+        lsFunc();
+    }
+
+    else {
         process.stdout.write('You typed: ' + cmd);
     }
-    
-    process.stdout.write('\ntype somthing else > ');
+
+    setTimeout(function() {process.stdout.write('\ntype somthing else > ')}, 1000);
 })
